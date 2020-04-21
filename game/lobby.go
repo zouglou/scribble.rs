511560f6701a2bc4c64083a8b3455361c4351638
BuildTools <unconfigured@null.spigotmc.org>
2020-04-21 02:12:10 +0200

    Don't display word to other when it's close (prevent mistyping frustration)

--- game/lobby.go
+++ game/lobby.go
@@ -213,9 +213,9 @@ func handleMessage(input string, sender *Player, lobby *Lobby) {
 			return
 		} else if levenshtein.ComputeDistance(normInput, normSearched) == 1 {
 			WriteAsJSON(sender, JSEvent{Type: "system-message", Data: fmt.Sprintf("'%s' is very close.", trimmed)})
+		} else {
+			sendMessageToAll(trimmed, sender, lobby)
 		}
-
-		sendMessageToAll(trimmed, sender, lobby)
 	}
 }
 
